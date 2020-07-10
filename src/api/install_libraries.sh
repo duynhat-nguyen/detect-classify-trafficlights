eee348aa1c634db3e7468cd127e4e1fd3c211d24
Nhat D. Nguyen <14892734+nhat-nguyenduy@users.noreply.github.com>
2020-07-10 15:50:59 +0700

    Create install_libraries.sh

---
+++ src/api/install_libraries.sh
@@ -0,0 +1,6 @@
+apt-get install protobuf-compiler python-pil python-lxml python-tk
+pip install --user Cython
+pip install --user contextlib2
+pip install --user jupyter
+pip install --user matplotlib
+pip install --user tf_slim

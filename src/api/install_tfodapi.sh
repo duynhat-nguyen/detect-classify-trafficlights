c52386ea1c4f9209941e9fbd8419f1b980bb5462
Nhat D. Nguyen <14892734+nhat-nguyenduy@users.noreply.github.com>
2020-07-10 19:54:44 +0700

    Update install_tfodapi.sh

--- src/api/install_tfodapi.sh
+++ src/api/install_tfodapi.sh
@@ -1,4 +1,4 @@
-cd ./src/api/
+mkdir ./src/api/tensorflow && cd ./src/api/tensorflow
 git clone https://github.com/tensorflow/models.git 
 cd models/research
 protoc object_detection/protos/*.proto --python_out=.

66692127a0f571092ef1581930117c25a86d8165
Nhat D. Nguyen <14892734+nhat-nguyenduy@users.noreply.github.com>
2020-07-10 16:55:52 +0700

    Update download_pretrained_models.sh

--- src/models/pretrained/download_pretrained_models.sh
+++ src/models/pretrained/download_pretrained_models.sh
@@ -1,1 +1,1 @@
-wget http://download.tensorflow.org/models/object_detection/ssd_inception_v2_coco_2018_01_28.tar.gz && tar -xvf ssd_inception_v2_coco_2018_01_28.tar.gz
+wget http://download.tensorflow.org/models/object_detection/ssd_inception_v2_coco_2018_01_28.tar.gz -P ./models/pretrained/ && tar -xvf ./models/pretrained/ssd_inception_v2_coco_2018_01_28.tar.gz

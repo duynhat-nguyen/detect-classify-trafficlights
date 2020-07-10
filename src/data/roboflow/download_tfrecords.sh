0b8385ab78c9e98403c227857d6225ee11867036
Nhat D. Nguyen <14892734+nhat-nguyenduy@users.noreply.github.com>
2020-07-10 22:13:18 +0700

    Update download_tfrecords.sh

--- src/data/roboflow/download_tfrecords.sh
+++ src/data/roboflow/download_tfrecords.sh
@@ -1,5 +1,5 @@
-mkdir -p ./src/tfrecord
-cd ./src/tfrecord
+mkdir -p ./data/tfrecord
+cd ./data/tfrecord
 curl -L "https://app.roboflow.ai/ds/poBN67PKDB?key=HbfSGuOP4v" > roboflow.zip
 unzip roboflow.zip
 rm roboflow.zip

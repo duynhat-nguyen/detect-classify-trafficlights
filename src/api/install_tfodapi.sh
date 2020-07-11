d2ac43ca971dc6a8b206bb716e78fb4e0ce217ba
Nhat <nhat.nguyenduy.sns@gmail.com>
2020-07-11 09:34:52 +0700

    Change to install TF2

--- src/api/install_tfodapi.sh
+++ src/api/install_tfodapi.sh
@@ -2,3 +2,6 @@ cd ./src/api
 git clone https://github.com/tensorflow/models.git 
 cd models/research
 protoc object_detection/protos/*.proto --python_out=.
+# Install TensorFlow Object Detection API.
+cp object_detection/packages/tf2/setup.py .
+python -m pip install .

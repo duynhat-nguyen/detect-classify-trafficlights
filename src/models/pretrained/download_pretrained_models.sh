e8f3ab975cb3939e340ecdefb893d10dca273dd9
Nhat D. Nguyen <14892734+nhat-nguyenduy@users.noreply.github.com>
2020-07-14 14:34:50 +0700

    Revert "Update download_pretrained_models.sh"
    This reverts commit cf9c9033c785a0dd82bc11da6a82b78990fd2d66.

--- src/models/pretrained/download_pretrained_models.sh
+++ src/models/pretrained/download_pretrained_models.sh
@@ -1,12 +1,9 @@
-mkdir -p ./models/pretrained/ssd
-mkdir ./models/pretrained/efficientdet
+mkdir -p ./models/pretrained && cd ./models/pretrained
 # SSD
-cd ./models/pretrained/ssd
 wget http://download.tensorflow.org/models/object_detection/ssd_inception_v2_coco_2018_01_28.tar.gz
 tar -xvf ssd_inception_v2_coco_2018_01_28.tar.gz
 rm ssd_inception_v2_coco_2018_01_28.tar.gz
 # EfficientDet
-cd ../efficientdet
 wget http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d2_coco17_tpu-32.tar.gz
 tar -xvf efficientdet_d2_coco17_tpu-32.tar.gz
 rm efficientdet_d2_coco17_tpu-32.tar.gz

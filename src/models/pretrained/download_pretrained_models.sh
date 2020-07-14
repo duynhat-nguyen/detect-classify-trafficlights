82eb882ec98228cbba55544683a5180f9743f3d2
Nhat <nhat.nguyenduy.sns@gmail.com>
2020-07-15 01:28:53 +0700

    Update

--- src/models/pretrained/download_pretrained_models.sh
+++ src/models/pretrained/download_pretrained_models.sh
@@ -1,5 +1,11 @@
-mkdir -p ./models/pretrained && cd ./models/pretrained
+mkdir -p ./models/pretrained/ssd
+mkdir ./models/pretrained/faster_rcnn
+cd ./models/pretrained/ssd
 # SSD
 wget http://download.tensorflow.org/models/object_detection/ssd_inception_v2_coco_2018_01_28.tar.gz
 tar -xvf ssd_inception_v2_coco_2018_01_28.tar.gz
 rm ssd_inception_v2_coco_2018_01_28.tar.gz
+# Faster R-CNN
+wget http://download.tensorflow.org/models/object_detection/faster_rcnn_inception_v2_coco_2018_01_28.tar.gz
+tar -xvf faster_rcnn_inception_v2_coco_2018_01_28.tar.gz
+rm faster_rcnn_inception_v2_coco_2018_01_28.tar.gz

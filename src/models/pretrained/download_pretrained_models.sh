2dcc9dfc9e9c211d2983ef26d36f1b468a5c4661
Nhat D. Nguyen <14892734+nhat-nguyenduy@users.noreply.github.com>
2020-07-14 14:45:13 +0700

    Update download_pretrained_models.sh

--- src/models/pretrained/download_pretrained_models.sh
+++ src/models/pretrained/download_pretrained_models.sh
@@ -3,7 +3,3 @@ mkdir -p ./models/pretrained && cd ./models/pretrained
 wget http://download.tensorflow.org/models/object_detection/ssd_inception_v2_coco_2018_01_28.tar.gz
 tar -xvf ssd_inception_v2_coco_2018_01_28.tar.gz
 rm ssd_inception_v2_coco_2018_01_28.tar.gz
-# EfficientDet
-wget http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d2_coco17_tpu-32.tar.gz
-tar -xvf efficientdet_d2_coco17_tpu-32.tar.gz
-rm efficientdet_d2_coco17_tpu-32.tar.gz

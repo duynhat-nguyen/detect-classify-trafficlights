200eb92db57df21335a66bf4651148ddb2d501b0
Nhat D. Nguyen <14892734+nhat-nguyenduy@users.noreply.github.com>
2020-07-22 16:38:48 +0700

    Update model_main.py

--- src/models/train/model_main.py
+++ src/models/train/model_main.py
@@ -59,7 +59,7 @@ FLAGS = flags.FLAGS
 def main(unused_argv):
   flags.mark_flag_as_required('model_dir')
   flags.mark_flag_as_required('pipeline_config_path')
-  config = tf.estimator.RunConfig(model_dir=FLAGS.model_dir, keep_checkpoint_max=None, save_checkpoints_steps=1000)
+  config = tf.estimator.RunConfig(model_dir=FLAGS.model_dir, keep_checkpoint_max=None, save_checkpoints_secs=227)
 
   train_and_eval_dict = model_lib.create_estimator_and_inputs(
       run_config=config,

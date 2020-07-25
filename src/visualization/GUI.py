9a4f2be8105fc46c116cb21a6333d1cddc6d0808
Nhat D. Nguyen <14892734+nhat-nguyenduy@users.noreply.github.com>
2020-07-25 15:26:21 +0700

    Update GUI.py

--- src/visualization/GUI.py
+++ src/visualization/GUI.py
@@ -5,6 +5,23 @@ import pandas as pd
 import numpy as np
 import altair as alt
 import os
+import numpy as np
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from distutils.version import StrictVersion
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+
+from object_detection.utils import ops as utils_ops
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
 
 def main():
     st.title("Traffic lights detection and classification")

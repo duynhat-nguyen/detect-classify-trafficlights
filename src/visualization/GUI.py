09258f45551b10481e252ffeaae30d6b9c41381d
Nhat D. Nguyen <14892734+nhat-nguyenduy@users.noreply.github.com>
2020-07-26 21:10:07 +0700

    Update GUI.py

--- src/visualization/GUI.py
+++ src/visualization/GUI.py
@@ -195,6 +195,8 @@ def inference(image_path):
     image_tmp = Image.open(image_path)
     # the array based representation of the image will be used later in order to prepare the
     # result image with boxes and labels on it.
+    image = image_tmp.resize((1280, 720), Image.ANTIALIAS)
+    
     image_np = load_image_into_numpy_array(image)
     # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
     image_np_expanded = np.expand_dims(image_np, axis=0)

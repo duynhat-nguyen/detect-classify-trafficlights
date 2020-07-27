990fe552bfe0cd8a3e4d97ed78291709e9b82981
Nhat D. Nguyen <14892734+nhat-nguyenduy@users.noreply.github.com>
2020-07-27 11:40:44 +0700

    Update download_pretrained_models.sh

--- src/models/pretrained/download_pretrained_models.sh
+++ src/models/pretrained/download_pretrained_models.sh
@@ -1,12 +1,13 @@
 mkdir -p ./models/pretrained/ssd
 mkdir ./models/pretrained/faster_rcnn
 cd ./models/pretrained/ssd
-# SSD
+
+# SSD Inception V2
 wget http://download.tensorflow.org/models/object_detection/ssd_inception_v2_coco_2018_01_28.tar.gz
 tar -xvf ssd_inception_v2_coco_2018_01_28.tar.gz
 rm ssd_inception_v2_coco_2018_01_28.tar.gz
-# Faster R-CNN
-cd ../faster_rcnn
-wget http://download.tensorflow.org/models/object_detection/faster_rcnn_inception_v2_coco_2018_01_28.tar.gz
-tar -xvf faster_rcnn_inception_v2_coco_2018_01_28.tar.gz
-rm faster_rcnn_inception_v2_coco_2018_01_28.tar.gz
+
+# SSD MobileNet V2
+wget http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v2_coco_2018_03_29.tar.gz
+tar -xvf ssd_mobilenet_v2_coco_2018_03_29.tar.gz
+rm ssd_mobilenet_v2_coco_2018_03_29.tar.gz

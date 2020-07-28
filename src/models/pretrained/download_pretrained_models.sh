d0a95f1828194ad5f6bd69b5fe9aa4d4ee256d74
Nhat D. Nguyen <14892734+nhat-nguyenduy@users.noreply.github.com>
2020-07-28 18:43:51 +0700

    Update download_pretrained_models.sh

--- src/models/pretrained/download_pretrained_models.sh
+++ src/models/pretrained/download_pretrained_models.sh
@@ -11,3 +11,8 @@ rm ssd_inception_v2_coco_2018_01_28.tar.gz
 wget http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v2_coco_2018_03_29.tar.gz
 tar -xvf ssd_mobilenet_v2_coco_2018_03_29.tar.gz
 rm ssd_mobilenet_v2_coco_2018_03_29.tar.gz
+
+# SSD ResNet 50 FPN
+wget http://download.tensorflow.org/models/object_detection/ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03.tar.gz
+tar -xvf ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03.tar.gz
+rm ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03.tar.gz
